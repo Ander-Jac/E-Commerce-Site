78cb5e391dde5fdb29cfab97a06c3c66db3db96f
Ander-Jac <jajake253@gmail.com>
2023-03-19 21:44:28 -0400

    added individual item pages and ui tweaks. Only cart functionality left.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React, { createElement } from "react";
+import React from "react"
 import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
+import { useEffect, useState } from "react";
 import "./App.css"
+
 
 const root = createRoot(document.getElementById("root"))
 
-class App extends React.Component {
-    render() {
-      return (
-        <div>
-            <NavBar/>
-            <HeroSection />
-            <FeaturedSection />
-            <MainFeatures />
-            <FeaturedSection2 />
-            <Footer />
-        </div>
-      );
-    }
-  };
 
-class IIP extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div>
-          <NavBar/>
-          <Footer />
+          <NavBar />
+          <HeroSection />
+          <FeaturedSection products={products} />
+          <MainFeatures />
+          <FeaturedSection products={products2} />
       </div>
     );
   }
@@ -33,16 +24,19 @@ class IIP extends React.Component {
 
 
 const NavBar = () => {
+  const handleItemClick = () => {
+    root.render(<App />);
+  };
     return (
        <nav id="nav-wrapper">
 
-        <div id="nav-logo"></div>
+        <div id="nav-logo" onClick={() => handleItemClick()}></div>
 
-        <ul id="nav-list">
-          <li className="nav-list-item">Shop</li>
-          <li className="nav-list-item">About</li>
-          <li className="nav-list-item">Contact</li>
-        </ul>
+        <div id="cart"></div>
+        <div id="cart-dropdown">
+          <ul id="cart-list"></ul>
+          <div id="cart-button"></div>
+        </div>
 
       </nav>
     )
@@ -59,52 +53,42 @@ const HeroSection = () => {
   )
 }
 
-const FeaturedSection = () => {
+const FeaturedSection = (props) => {
+  const products = props.products
+  const [items, setItems] = useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function populateSection() {
-      let counter = 0;
-      products.forEach(item => {
-        const featuredWrapper = document.getElementById("featured-section-wrapper")
-
-        /* Creating Elements */
-        const box = document.createElement("div");
-        box.classList.add("featured-item")
-
-        const image = document.createElement("div");
-        image.classList.add("featured-item-image");
-        image.setAttribute("id", "featured-image-" + item.id)
-
-        const description = document.createElement("div");
-        description.classList.add("featured-item-description");
-
-        const name = document.createElement("div");
-        name.classList.add("featured-item-name");
-        name.innerHTML = products[counter].name
-
-        const price = document.createElement("div");
-        price.classList.add("featured-item-price");
-        price.innerHTML = products[counter].price
-
-        /* Appending Elements */
-        description.append(name)
-        description.append(price)
-
-        box.append(image)
-        box.append(description)
-        console.log(box)
-
-        featuredWrapper.append(box)
-        console.log(featuredWrapper)
-        counter++;
-      });
+      setItems(products)
     }
-    populateSection()
-  })
-  return(
-    <section id="featured-section-wrapper"></section>
-  )
-}
+    populateSection();
+  }, []);
+
+  const handleItemClick = (item) => {
+    root.render(<IIP info={item} />);
+  };
+
+  return (
+    <section id="featured-section-wrapper">
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className="featured-item"
+          onClick={() => handleItemClick(item)}
+        >
+          <div
+            className="featured-item-image"
+            id={`featured-image-${item.id}`}
+          ></div>
+          <div className="featured-item-description">
+            <div className="featured-item-name">{item.name}</div>
+            <div className="featured-item-price">{item.price}</div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
 
 const MainFeatures = () => {
   return(
@@ -121,42 +105,7 @@ const MainFeatures = () => {
   )
 }
 
-const FeaturedSection2 = () => {
-  return(
-    <section id="featured-section-wrapper">
 
-      <div id="" className="featured-item">
-        <div id="featured2-image-1" className="featured-item-image"></div>
-        <div className="featured-item-description">
-          <div className="featured-item-name">ELEMENT | 43MM</div>
-          <div className="featured-item-price">$171</div>
-        </div>
-      </div>
-      <div id="" className="featured-item">
-        <div id="featured2-image-2" className="featured-item-image"></div>
-        <div className="featured-item-description">
-          <div className="featured-item-name">CHRONO CERAMIC | 45MM</div>
-          <div className="featured-item-price">$298</div>
-        </div>
-      </div>
-      <div id="" className="featured-item">
-        <div id="featured2-image-3" className="featured-item-image"></div>
-        <div className="featured-item-description">
-          <div className="featured-item-name">AIRHAWK | 42MM</div>
-          <div className="featured-item-price">$228</div>
-        </div>
-      </div>
-      <div id="" className="featured-item">
-        <div id="featured2-image-4" className="featured-item-image"></div>
-        <div className="featured-item-description">
-          <div className="featured-item-name">FIELD | 41MM</div>
-          <div className="featured-item-price">$118</div>
-        </div>
-      </div>
-
-    </section>
-  )
-}
 
 const Footer = () => {
   return (
@@ -171,6 +120,73 @@ const Footer = () => {
     </footer>
   )
 }
+
+
+const IIP = (props) => {
+  React.useEffect(() => {
+  })
+  return (
+      <div>
+          <NavBar />
+          <IIPMainContent info={props.info} />
+      </div>
+  );
+}
+
+const IIPMainContent = (props) => {
+  const { info } = props;
+  
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [des, setDes] = useState("");
+  
+  /* Event Handler for Cart Add */
+  const cartList = document.getElementById("cart-list");
+  const handleItemClick = () => {
+  const itemName = document.createElement("div");
+  itemName.append(name);
+  itemName.classList.add("cart-item-name");
+
+  const x = document.createElement("div");
+  x.innerHTML = "X";
+  x.classList.add("cart-x");
+
+  const wrapper = document.createElement("li");
+  wrapper.append(itemName);
+  wrapper.append(x);
+  wrapper.classList.add("cart-item-wrapper")
+
+  cartList.append(wrapper);
+};
+
+  useEffect(() => {
+    async function populateSection() {
+      setId(info.id);
+      setName(info.name);
+      setPrice(info.price);
+      setDes(info.description);
+    }
+    populateSection();
+  }, [info]);
+
+  return (
+    <section id="IIP-main-content-wrapper">
+      <div id={`featured-image-${id}`} className="IIP-image"></div>
+      <div id="IIP-info">
+        <div>
+          <h1 id="IIP-name">{name}</h1>
+          <h3 id="IIP-price">{price}</h3>
+        </div>
+        <p id="IIP-description">{des}</p>
+        <button id="IIP-button" onClick={() => handleItemClick()}>Add To Cart | {price}</button>
+      </div>
+    </section>
+  );
+};
+
+
+
 
 root.render (
   <App />
@@ -212,28 +228,28 @@ const products2 = [
   id: 5,
     name: "ELEMENT | 43MM",
     price: "$171",
-    imgUrl: "/src/images/featured2-image-1.jpg",
+    imgUrl: "/src/images/featured-image-5.jpg",
     description: "Dress in rare form. The Element Ceramic watch is designed with the smooth, highly resistant sheen of engineering ceramic, featuring an original gloss black tone."
   },
   {
     id: 6,
     name: "CHRONO CERAMIC | 45MM",
     price: "$298",
-    imgUrl: "/src/images/featured2-image-2.jpg",
+    imgUrl: "/src/images/featured-image-6.jpg",
     description: "Take a trip up the PCH to the Bay, and you’ll see this vibey shade of grey hangin’ around. The Fog Grey colorway brings a smooth sky grey tone to our brand-favorite ceramics line. And it’s a perfect match for our new Petal Blush ceramic colorway."
   },
   {
     id: 7,
     name: "AIRHAWK | 42MM",
     price: "$228",
-    imgUrl: "/src/images/featured2-image-3.jpg",
+    imgUrl: "/src/images/featured-image-7.jpg",
     description: "The Airhawk transforms pioneering aircraft design into watch form. Its clean dial reads easy like a proper pilot watch should and features multi-function sub dials for some extra tactically in your timekeeping experience. The Honey Smoke colorway balances cool stainless steel with pale gold accents and pops of MVMT blue."
   },
   {
     id: 8,
     name: "FIELD | 41MM",
     price: "$118",
-    imgUrl: "/src/images/featured2-image-4.jpg",
+    imgUrl: "/src/images/featured-image-8.jpg",
     description: "Have a field day. Inspired by the outdoors and the gear to get you there, the Field sports our first fixed number dial and a water resistant 41mm case. The Everest Blue features a rich all-blue colorway."
   }
 ]
